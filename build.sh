8aa35354c1465087b6e704e97798a5e77284af16
Robb Kidd <robbkidd@honeycomb.io>
2021-07-09 14:19:45 -0400

    set the version number at build time
    Use the tag if set by CircleCI or fallback to computing a dev version
    from proximity to most recent tag in git.

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 
 set -e
 
-./test.sh
-
-VERSION=2.2.1
+# A fallback version when run outside of CI. Will look like: 2.2.1-13-gd591456
+# 2.2.1 - most recent tag with the leading v trimmed
+# 13 - no.of commits away from tag
+# gd591456 - this commit's id
+GIT_VERSION="`git describe | sed -e s/^v//`"
+VERSION="${CIRCLE_TAG:-$GIT_VERSION}"
 REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1"
 
 ROOT_DIR=$(pwd)
@@ -16,7 +19,7 @@ HANDLERS="cloudwatch-handler s3-handler sns-handler mysql-handler postgresql-han
 
 for HANDLER in ${HANDLERS}; do
 	cd ${HANDLER}
-	GOOS=linux go build
+	GOOS=linux go build -ldflags "-X github.com/honeycombio/agentless-integrations-for-aws/common.version=${VERSION}"
 	cd ${ROOT_DIR}
 	mv ${HANDLER}/${HANDLER} pkg
 done;
