976998902379a3b324c789cb8d8ca6b8f52b4c4c
Robb Kidd <robbkidd@honeycomb.io>
2021-07-09 14:37:10 -0400

    separate building and publishing

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# packages handlers and ships them to S3 for use in templates
+# packages handlers
 
 set -e
 
@@ -9,7 +9,6 @@ set -e
 # gd591456 - this commit's id
 GIT_VERSION="`git describe | sed -e s/^v//`"
 VERSION="${CIRCLE_TAG:-$GIT_VERSION}"
-REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1"
 
 ROOT_DIR=$(pwd)
 rm -rf pkg
@@ -27,19 +26,3 @@ done;
 cd ./pkg
 
 zip ingest-handlers.zip *
-
-for REGION in ${REGIONS}; do
-	DEPLOY_ROOT=s3://honeycomb-integrations-${REGION}/agentless-integrations-for-aws
-	aws s3 cp ingest-handlers.zip ${DEPLOY_ROOT}/LATEST/ingest-handlers.zip
-	aws s3 cp ingest-handlers.zip ${DEPLOY_ROOT}/${VERSION}/ingest-handlers.zip
-done;
-
-cd ${ROOT_DIR}
-
-# publish the templates to our builds bucket
-DEPLOY_ROOT=s3://honeycomb-builds/honeycombio/integrations-for-aws
-
-for TEMPLATE in templates/*; do
-	aws s3 cp ${TEMPLATE} ${DEPLOY_ROOT}/LATEST/${TEMPLATE}
-	aws s3 cp ${TEMPLATE} ${DEPLOY_ROOT}/${VERSION}/${TEMPLATE}
-done
