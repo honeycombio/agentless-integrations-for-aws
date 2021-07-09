f67ab48d07983e78ea929fbc05e86eae9d9c9333
Robb Kidd <robbkidd@honeycomb.io>
2021-07-09 13:57:17 -0400

    update CI to validate CloudFormation templates
    - sets our useful filters with names
    - renames the run_tests job to make it clear those are only Go tests

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 # quickly validate that templates are valid
 
 set -e
-
+export AWS_PAGER=""
 for TEMPLATE in templates/*.yml; do
-    aws cloudformation validate-template --template-body=file://./${TEMPLATE}
+    aws cloudformation validate-template --template-body=file://./${TEMPLATE} \
+    && echo "${TEMPLATE} parses OK."
 done;
