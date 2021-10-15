ce52b29a95a4c6adb768feb4ac889197a95559d8
JamieDanielson <jamieedanielson@gmail.com>
2021-10-15 17:10:45 -0400

    Revert "fix kms secret decryption" (#68)
    This reverts commit aa46d170f22f645d0249c4de6040d03d21f2ba5b.

--- common/common.go
+++ common/common.go
@@ -70,7 +70,7 @@ func InitHoneycombFromEnvVars() error {
 				logrus.WithError(err).Error("unable to decrypt honeycomb write key")
 				return fmt.Errorf("unable to decrypt honeycomb write key")
 			}
-			writeKey = base64.StdEncoding.EncodeToString(resp.Plaintext)
+			writeKey = string(resp.Plaintext)
 		}
 	} else {
 		writeKey = os.Getenv("HONEYCOMB_WRITE_KEY")
