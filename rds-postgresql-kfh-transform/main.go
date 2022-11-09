7b741b67d840ea9c57ddc3e1256262c735dbc2e4
Brooke Sargent <brookesargent@honeycomb.io>
2022-11-09 11:27:11 -0500

    RDS Lambda transforms (#160)
    ## Short description of the changes
    This PR adds support for transforming a Kinesis Firehose payload with
    RDS MySQL or Postgresql Cloudwatch logs into a parsed event that can be
    sent on to Honeycomb.
    
    ## How to test
    This is new functionality and the Terraform module that makes this
    easier to test is still
    [WIP](https://github.com/honeycombio/terraform-aws-integrations/pull/7).
    The infrastructure needed is an RDS MySQL or Postgres database with log
    exports enabled, a Kinesis Firehose that receives data from the RDS
    CloudWatch log group(s) and sends to the Honeycomb kinesis_events
    endpoint with this Lambda enabled as the transformation option.

---
+++ rds-postgresql-kfh-transform/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/honeycombio/agentless-integrations-for-aws/common"
+	"github.com/honeycombio/honeytail/event"
+	"github.com/honeycombio/honeytail/parsers/postgresql"
+)
+
+var parser *postgresql.Parser
+
+func handler(ctx context.Context, input events.KinesisFirehoseEvent) (events.KinesisFirehoseResponse, error) {
+	if parser == nil {
+		return events.KinesisFirehoseResponse{}, fmt.Errorf("parser not initialized, cannot process events")
+	}
+
+	var response events.KinesisFirehoseResponse
+	for _, record := range input.Records {
+		cwb, err := decodeData(record.Data)
+		if err != nil {
+			return events.KinesisFirehoseResponse{}, err
+		}
+
+		// these messages ensure kinesis can reach the lambda and don't require processing
+		if cwb.MessageType == "CONTROL_MESSAGE" {
+			var droppedRecord events.KinesisFirehoseResponseRecord
+			droppedRecord.RecordID = record.RecordID
+			droppedRecord.Result = events.KinesisFirehoseTransformedStateDropped
+			response.Records = append(response.Records, droppedRecord)
+		} else if cwb.MessageType == "DATA_MESSAGE" {
+			var parsedEventJson []map[string]interface{}
+			for _, v := range cwb.LogEvents {
+				lines := make(chan string)
+				parsedEvents := make(chan event.Event)
+				go func() {
+					messageLines := strings.Split(v.Message, "\n")
+					for _, l := range messageLines {
+						lines <- l
+					}
+					close(lines)
+				}()
+
+				go func() {
+					parser.ProcessLines(lines, parsedEvents, nil)
+					close(parsedEvents)
+				}()
+
+				for p := range parsedEvents {
+					hnyEvent := p.Data
+					hnyEvent["aws.cloudwatch.owner"] = cwb.Owner
+					hnyEvent["aws.cloudwatch.group"] = cwb.LogGroup
+					hnyEvent["aws.cloudwatch.stream"] = cwb.LogStream
+					hnyEvent["aws.cloudwatch.id"] = v.ID
+					hnyEvent["timestamp"] = p.Timestamp
+					parsedEventJson = append(parsedEventJson, hnyEvent)
+				}
+			}
+
+			b, err := json.Marshal(parsedEventJson)
+			if err != nil {
+				return events.KinesisFirehoseResponse{}, err
+			}
+
+			var transformedRecord events.KinesisFirehoseResponseRecord
+			transformedRecord.RecordID = record.RecordID
+			transformedRecord.Result = events.KinesisFirehoseTransformedStateOk
+			transformedRecord.Data = b
+			response.Records = append(response.Records, transformedRecord)
+		}
+	}
+	return response, nil
+}
+
+func decodeData(data []byte) (events.CloudwatchLogsData, error) {
+	var cwb events.CloudwatchLogsData
+
+	// kinesis firehose send data payloads gzipped
+	gr, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return events.CloudwatchLogsData{}, err
+	}
+	gr.Multistream(false)
+	defer gr.Close()
+
+	decoder := json.NewDecoder(gr)
+	err = decoder.Decode(&cwb)
+	if err != nil && err != io.EOF {
+		return events.CloudwatchLogsData{}, err
+	}
+	return cwb, nil
+}
+
+func main() {
+	parser = &postgresql.Parser{}
+	parser.Init(&postgresql.Options{LogLinePrefix: "%t:%r:%u@%d:[%p]:"})
+	common.AddUserAgentMetadata("rds", "postgresql")
+	lambda.Start(handler)
+}
