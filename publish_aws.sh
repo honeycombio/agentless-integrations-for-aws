a379b9ece8cbe9bcdc0a3f341e0cf99da05ade77
Brooke Sargent <brookesargent@honeycomb.io>
2023-10-13 09:50:00 -0400

    fix: update publish-aws script for both archs (#221)
    ## Which problem is this PR solving?
    
    Follow up to #220 to make the publish-aws script work for both
    architectures.
    
    ## Short description of the changes
    
    Adjusts the script to copy both amd64 and arm64 zips for each handler to
    S3. I ran the build script locally and the publish script locally and it
    worked.

--- publish_aws.sh
+++ publish_aws.sh
@@ -14,21 +14,22 @@ HANDLERS="cloudwatch-handler s3-handler sns-handler mysql-handler postgresql-han
 # if DRYRUN is set to anything, turn it into the awscli switch
 [[ -n "${DRYRUN}" ]] && DRYRUN="--dryrun"
 
+ZIP_PATH="./pkg"
+
+if [[ -d "${ZIP_PATH}" ]]; then
+  echo "+++ Publishing ${ZIP_PATH} to S3"
+else
+  echo 1>&2 "$ZIP_PATH does not exist. Run build.sh?"
+  exit 1
+fi
+
 echo "+++ Uploading handlers"
 for HANDLER in ${HANDLERS}; do
-  ZIP_NAME="${HANDLER}.zip"
-  ZIP_PATH="./pkg/${ZIP_NAME}"
-
-  if [[ -f "${ZIP_PATH}" ]]; then
-    echo "+++ Publishing ${ZIP_PATH} to S3"
-  else
-    echo 1>&2 "$ZIP_PATH does not exist. Run build.sh?"
-  	exit 1
-  fi
-
   for REGION in ${REGIONS}; do
     DEPLOY_ROOT=s3://honeycomb-integrations-${REGION}/agentless-integrations-for-aws
-    aws s3 cp ${DRYRUN} ${ZIP_PATH} ${DEPLOY_ROOT}/${VERSION}/${ZIP_NAME}
-    [[ -n "$CIRCLE_TAG" ]] && aws s3 cp ${DRYRUN} ${ZIP_PATH} ${DEPLOY_ROOT}/LATEST/${ZIP_NAME} || true
+    aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-amd64.zip ${DEPLOY_ROOT}/${VERSION}/${HANDLER}-amd64.zip
+    [[ -n "$CIRCLE_TAG" ]] && aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-amd64.zip ${DEPLOY_ROOT}/LATEST/${HANDLER}-amd64.zip || true
+    aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-arm64.zip ${DEPLOY_ROOT}/${VERSION}/${HANDLER}-arm64.zip
+    [[ -n "$CIRCLE_TAG" ]] && aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-arm64.zip ${DEPLOY_ROOT}/LATEST/${HANDLER}-arm64.zip || true
   done
 done
