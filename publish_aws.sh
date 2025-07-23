2a0a8b89beabc88d3cfe383c9df533f350bfaba0
Irving Popovetsky <irving@honeycomb.io>
2025-07-23 07:58:58 -0700

    Update publish regions list and make it more readable
    Signed-off-by: Irving Popovetsky <irving@honeycomb.io>

--- publish_aws.sh
+++ publish_aws.sh
@@ -8,8 +8,63 @@ set -e
 # gd591456 - this commit's id
 GIT_VERSION="`git describe | sed -e s/^v//`"
 VERSION="${CIRCLE_TAG:-$GIT_VERSION}"
-REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1"
-HANDLERS="cloudwatch-handler s3-handler sns-handler mysql-handler postgresql-handler publisher rds-mysql-kfh-transform rds-postgresql-kfh-transform sns-kfh-transform"
+REGIONS=(
+  # US Regions
+  "us-east-1"      # N. Virginia
+  "us-east-2"      # Ohio
+  "us-west-1"      # N. California
+  "us-west-2"      # Oregon
+
+  # Canada Regions
+  "ca-central-1"   # Central
+  "ca-west-1"      # Calgary
+
+  # South America Regions
+  "sa-east-1"      # São Paulo
+
+  # Europe Regions
+  "eu-central-1"   # Frankfurt
+  "eu-central-2"   # Zurich
+  "eu-west-1"      # Ireland
+  "eu-west-2"      # London
+  "eu-west-3"      # Paris
+  "eu-north-1"     # Stockholm
+  "eu-south-1"     # Milan
+  "eu-south-2"     # Spain
+
+  # Asia Pacific Regions
+  "ap-northeast-1" # Tokyo
+  "ap-northeast-2" # Seoul
+  "ap-northeast-3" # Osaka
+  "ap-south-1"     # Mumbai
+  "ap-south-2"     # Hyderabad
+  "ap-southeast-1" # Singapore
+  "ap-southeast-2" # Sydney
+  "ap-southeast-3" # Jakarta
+  "ap-southeast-4" # Melbourne
+  "ap-southeast-5" # Malaysia
+
+  # Middle East Regions
+  "me-south-1"     # Bahrain
+  "me-central-1"   # UAE
+
+  # Israel Region
+  "il-central-1"   # Tel Aviv
+
+  # Africa Region
+  "af-south-1"     # Cape Town
+)
+HANDLERS=(
+  "cloudwatch-handler"
+  "s3-handler"
+  "sns-handler"
+  "mysql-handler"
+  "postgresql-handler"
+  "publisher"
+  "rds-mysql-kfh-transform"
+  "rds-postgresql-kfh-transform"
+  "sns-kfh-transform"
+)
 
 # if DRYRUN is set to anything, turn it into the awscli switch
 [[ -n "${DRYRUN}" ]] && DRYRUN="--dryrun"
@@ -24,8 +79,8 @@ else
 fi
 
 echo "+++ Uploading handlers"
-for HANDLER in ${HANDLERS}; do
-  for REGION in ${REGIONS}; do
+for HANDLER in "${HANDLERS[@]}"; do
+  for REGION in "${REGIONS[@]}"; do
     DEPLOY_ROOT=s3://honeycomb-integrations-${REGION}/agentless-integrations-for-aws
     aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-amd64.zip ${DEPLOY_ROOT}/${VERSION}/${HANDLER}-amd64.zip
     [[ -n "$CIRCLE_TAG" ]] && aws s3 cp ${DRYRUN} ${ZIP_PATH}/${HANDLER}-amd64.zip ${DEPLOY_ROOT}/LATEST/${HANDLER}-amd64.zip || true
